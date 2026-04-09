8a59dac50a5c066ea9ac09e0b73d7ee5d1b684f2
Martyna Kochalska <pyskaa0506@gmail.com>
2026-04-09 21:01:59 +0200

    Added Perspective Projection

--- 1st_part-3D_camera/camera.py
+++ 1st_part-3D_camera/camera.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy as np
 import sys
+import json
 
 pygame.init()
 WIDTH, HEIGHT = 800, 600
@@ -9,13 +10,40 @@ pygame.display.set_caption("Kamera 3D")
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+LIME = (0, 255, 0)
+
+zoom = 400
+
+with open('scene.json', 'r') as scene_file:
+    scene_data = json.load(scene_file)
+
+vertices = np.array(scene_data['vertices'], dtype=np.float32)
+edges = scene_data['edges']
+
 
 def draw_scene():
     screen.fill(BLACK)
 
-    pygame.draw.line(screen, WHITE, (400,300), (500, 400), 2)
-    pygame.display.flip()
+    for edge in edges:
+        v1 = vertices[edge[0]]
+        v2 = vertices[edge[1]]
 
+        z1 = v1[2] if v1[2] != 0 else 0.0001
+        z2 = v2[2] if v2[2] != 0 else 0.0001
+
+        x1_proj = v1[0] * zoom / z1
+        y1_proj = v1[1] * zoom / z1
+
+        x2_proj = v2[0] * zoom / z2
+        y2_proj = v2[1] * zoom / z2
+
+        x1_resized = int(x1_proj + WIDTH / 2)
+        y1_resized = int(-y1_proj + HEIGHT / 2)
+        x2_resized = int(x2_proj + WIDTH / 2)
+        y2_resized = int(-y2_proj + HEIGHT / 2)
+
+        pygame.draw.line(screen, LIME, (x1_resized, y1_resized), (x2_resized, y2_resized), 2)
+    pygame.display.flip()
 draw_scene()
 
 running = True
